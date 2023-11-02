3bb6179bc07725fe4f209fd91e7932d39183106b
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-02 14:42:06 +0100

    main view added

---
+++ src/main/java/pl/szymontruszkowski/employeedirectory/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package pl.szymontruszkowski.employeedirectory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
