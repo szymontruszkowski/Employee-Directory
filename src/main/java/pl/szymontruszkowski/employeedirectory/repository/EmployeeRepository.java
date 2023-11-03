1bc9efc8bded648630c855aa7f5f0d734bdf594b
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-03 09:23:33 +0100

    comments added in EmployeeRepository

--- src/main/java/pl/szymontruszkowski/employeedirectory/repository/EmployeeRepository.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/repository/EmployeeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.szymontruszkowski.employeedirectory.model.Employee;
 
+/**
+ * Repository class that will be handling H2 Database operations.
+ */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
