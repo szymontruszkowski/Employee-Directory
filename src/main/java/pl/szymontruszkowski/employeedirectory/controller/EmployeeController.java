4f2e141ddbac0a542b38b5614b775c01e931ca80
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-06 12:10:25 +0100

    main page method updated in EmployeeController

--- src/main/java/pl/szymontruszkowski/employeedirectory/controller/EmployeeController.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/controller/EmployeeController.java
@@ -1,13 +1,32 @@
 package pl.szymontruszkowski.employeedirectory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.szymontruszkowski.employeedirectory.model.Employee;
+import pl.szymontruszkowski.employeedirectory.service.EmployeeService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model theModel) {
+
+        List<Employee> theEmployees = employeeService.findAll();
+
+        theModel.addAttribute("employees", theEmployees);
+
         return "main";
     }
 
