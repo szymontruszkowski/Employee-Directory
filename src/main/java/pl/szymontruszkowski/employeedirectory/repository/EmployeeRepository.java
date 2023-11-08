8060958715c32a07b8c7238eb1d04182252864ee
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-08 12:56:45 +0100

    EmployeeRepository updated with sorting methods

--- src/main/java/pl/szymontruszkowski/employeedirectory/repository/EmployeeRepository.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/repository/EmployeeRepository.java
@@ -4,9 +4,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.szymontruszkowski.employeedirectory.model.Employee;
 
+import java.util.List;
+
 /**
  * Repository class that will be handling H2 Database operations.
  */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    /**
+     * Find all employees stored in H2 Database and sort them by first name ascending.
+     * @return  list of employees sorted by first name
+     */
+    List<Employee> findAllByOrderByFirstNameAsc();
+
+    /**
+     * Find all employees stored in H2 Database and sort them by last name ascending.
+     * @return  list of employees sorted by last name
+     */
+    List<Employee> findAllByOrderByLastNameAsc();
 }
