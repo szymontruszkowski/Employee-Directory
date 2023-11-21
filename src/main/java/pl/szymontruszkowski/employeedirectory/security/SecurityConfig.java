62564192bf3056219488aa19fb6c4535d5257113
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-21 10:30:31 +0100

    users and roles defined in SecurityConfig class

--- src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
@@ -1,7 +1,35 @@
 package pl.szymontruszkowski.employeedirectory.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails lucas = User.builder()
+                .username("lucas")
+                .password("{noop}lucas123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails william = User.builder()
+                .username("william")
+                .password("{noop}william123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails emma = User.builder()
+                .username("emma")
+                .password("{noop}emma123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(lucas, william, emma);
+    }
 }
