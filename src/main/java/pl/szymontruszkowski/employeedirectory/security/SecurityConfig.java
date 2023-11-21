ee4054fc69aa51c623b0f3ea59728a1bf978bd88
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-21 13:47:43 +0100

    SecurityConfig modified to reference custom login form

--- src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package pl.szymontruszkowski.employeedirectory.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,5 +33,19 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(lucas, william, emma);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                                .anyRequest().authenticated())
+
+                                .formLogin(form -> form
+                                    .loginPage("/showLoginPage")
+                                    .loginProcessingUrl("/authenticateTheUser")
+                                    .permitAll());
+
+        return http.build();
     }
 }
