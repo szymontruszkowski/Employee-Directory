774aabfa3d7a04359a97ab12835bff16eab9a09f
Szymon Truszkowski <struszkowski98@gmail.com>
2023-11-27 14:28:12 +0100

    unnecessary import deleted

--- src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
+++ src/main/java/pl/szymontruszkowski/employeedirectory/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.szymontruszkowski.employeedirectory.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
